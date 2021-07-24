24de0a2d7597f5ed8734a1158deb927db60b7d1f
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2021-07-24 15:11:43 +0300

    ci

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -3,6 +3,8 @@ cd vk
 wget https://sdk.lunarg.com/sdk/download/latest/linux/vulkan-sdk.tar.gz
 tar -xf archive.tar.gz
 
+apt-get install tar
+
 for file in . do                    
   tar -xf "$file"
 done
