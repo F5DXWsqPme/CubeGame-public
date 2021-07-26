aca58dcc79c501c9f751c7c320228064f1950baa
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2021-07-26 11:55:28 +0300

    ci

--- .github/workflows/install-vulkan.sh
+++ .github/workflows/install-vulkan.sh
@@ -3,25 +3,23 @@ cd vk
 
 wget https://sdk.lunarg.com/sdk/download/latest/linux/vulkan-sdk.tar.gz
 
-cd ..
-
-for file in vk
+for file in ~/vk
 do
   echo "$file in vk"
   if [ -f "$file" ]
   then
     echo "$file downloaded"
     tar -xzf "$file"
+    break
   fi
 done
 
-for dir in vk
+for dir in ~/vk
 do
   echo "$dir in vk"
   if [ -d "$dir" ]
   then
     echo "$dir extracted"
-    cd vk
     cd "$dir"
     break
   fi
