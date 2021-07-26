cc5aa7b303043e7ecac8a62d3c3e4b32e72f26c3
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2021-07-26 13:05:43 +0300

    ci

--- .github/workflows/install-vulkan.sh
+++ .github/workflows/install-vulkan.sh
@@ -1,28 +1,31 @@
-sudo mkdir vk
-cd vk
+#sudo mkdir vk
+#cd vk
+#
+#sudo wget --no-verbose https://sdk.lunarg.com/sdk/download/latest/linux/vulkan-sdk.tar.gz
+#
+#for file in *
+#do
+#  echo "$file in vk"
+#  if [ -f "$file" ]
+#  then
+#    echo "$file downloaded"
+#    sudo tar -xzf "$file"
+#    break
+#  fi
+#done
+#
+#for dir in *
+#do
+#  echo "$dir in vk"
+#  if [ -d "$dir" ]
+#  then
+#    echo "$dir extracted"
+#    cd "$dir"
+#    break
+#  fi
+#done
+#
+#sudo ./vulkansdk
 
-sudo wget --no-verbose https://sdk.lunarg.com/sdk/download/latest/linux/vulkan-sdk.tar.gz
-
-for file in *
-do
-  echo "$file in vk"
-  if [ -f "$file" ]
-  then
-    echo "$file downloaded"
-    sudo tar -xzf "$file"
-    break
-  fi
-done
-
-for dir in *
-do
-  echo "$dir in vk"
-  if [ -d "$dir" ]
-  then
-    echo "$dir extracted"
-    cd "$dir"
-    break
-  fi
-done
-
-./vulkansdk
+sudo apt install vulkan-utils
+sudo apt install libvulkan1 mesa-vulkan-drivers vulkan-utils
